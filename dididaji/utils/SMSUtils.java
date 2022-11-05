d87a7728992ae114c0b207159647a2c918be8092
TECNB <106745809+TECNB@users.noreply.github.com>
2022-11-05 20:18:59 +0800

    Update SMSUtils.java

--- dididaji/utils/SMSUtils.java
+++ dididaji/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tDiWAvVFSRrtBkokUH7", "gOiHd2EAydDOTwJRED3NCpSN5Nz6vO");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
